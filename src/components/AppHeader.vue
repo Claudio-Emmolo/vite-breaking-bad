9332d6ed31bd512223cbd23a04386acdfe5b1218
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-11 14:55:55 +0100

    Generated API and saved on store.js

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,38 @@
 <script>
+import { store } from '../store.js';
+import axios from 'axios';
+
+
+
 export default {
     name: 'AppHeader',
+    data() {
+        return {
+            store,
+            apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0',
+        }
+    },
+
+    methods: {
+        getApiElement() {
+
+            axios.get(this.apiUrl, {
+                params: {
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.data);
+                    this.store = response.data.data
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    },
+
+    created() {
+        this.getApiElement()
+    },
 
 }
 </script>
