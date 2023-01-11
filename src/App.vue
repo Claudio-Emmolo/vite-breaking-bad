fe2e567a775d2c8485cb6b6593c5ef6577104d42
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-11 14:32:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+// Import style / framework
+@use 'bootstrap/scss/bootstrap.scss' as *;
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+
+// Style
+</style>
