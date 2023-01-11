46c2002804afd31708502cf8c5a555f3213a3849
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-11 14:41:32 +0100

    Create header and main components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
 </script>
 
 <template>
-
+  <!-- Import Header -->
+  <AppHeader />
+  <!-- Import Main -->
+  <AppMain />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'AppHeader',
+
+}
+</script>
+
+<template>
+    <header>
+        <div class="container-fluid">
+            <h1 class="py-3">Yu-Gi-Oh</h1>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
